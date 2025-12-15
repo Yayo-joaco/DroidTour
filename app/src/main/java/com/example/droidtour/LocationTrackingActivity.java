7c8ddcec4313c81536d6ae712ca355a45736dc5f
AlpacaTaka <109617843+AlpacaTaka@users.noreply.github.com>
2025-12-15 15:58:43 -0500

    mejoras visuales

--- app/src/main/java/com/example/droidtour/LocationTrackingActivity.java
+++ app/src/main/java/com/example/droidtour/LocationTrackingActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
@@ -32,7 +33,7 @@ public class LocationTrackingActivity extends AppCompatActivity implements OnMap
     private GoogleMap mMap;
     private TextView tvTourName, tvPointsCompleted;
     private MaterialButton btnShowStops;
-    private android.widget.ProgressBar progressLoading;
+    private View progressLoading;
     private com.example.droidtour.utils.PreferencesManager prefsManager;
     private FirestoreManager firestoreManager;
     private FirebaseFirestore db;
